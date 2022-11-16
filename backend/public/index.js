b0b906a758d1285629662c82bdc24d53b4a12d3f
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-11-16 14:27:06 -0300

    feat: Start Project: basic config

---
+++ backend/public/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = exports.App = void 0;
+const express_1 = __importDefault(require("express"));
+require("express-async-errors");
+const cors_1 = __importDefault(require("cors"));
+// import loginRouter from './routes/loginRouter';
+class App {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.app.use((0, cors_1.default)());
+        this.config();
+        // this.app.use('/login', loginRouter);
+        this.app.get('/', (req, res) => res.json({ ok: true }));
+    }
+    config() {
+        const accessControl = (_req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+            res.header('Access-Control-Allow-Headers', '*');
+            next();
+        };
+        this.app.use(express_1.default.json());
+        this.app.use(accessControl);
+    }
+    start(PORT) {
+        this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+    }
+}
+exports.App = App;
+exports.app = new App().app;
