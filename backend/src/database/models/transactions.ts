84a983a81d361cb26017eb697c444ebdf862e8a9
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-11-20 10:35:26 -0300

    feat(database): Create models sequelize

---
+++ backend/src/database/models/transactions.ts
@@ -0,0 +1,48 @@
+import { INTEGER, Model, DATE } from 'sequelize';
+import db from '.';
+import Account from './accounts';
+
+class Transaction extends Model {
+  id!: number;
+  debitedAccountId!: number;
+  creditedAccountId!: number;
+  value!: number;
+  createdAt!: Date;
+}
+
+Transaction.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  debitedAccountId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  creditedAccountId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  value: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DATE,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'transactions',
+  underscored: true,
+});
+
+Account.hasMany(Transaction, { foreignKey: 'debitedAccountId', as: 'debitedAccountId' });
+Transaction.belongsTo(Account, { foreignKey: 'debitedAccountId', as: 'debited' });
+
+Account.hasMany(Transaction, { foreignKey: 'creditedAccountId', as: 'creditedAccountId' });
+Transaction.belongsTo(Account, { foreignKey: 'creditedAccountId', as: 'credited' });
+
+export default Transaction;
---
+++ backend/src/database/models/users.ts
@@ -0,0 +1,39 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+import Account from './accounts';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  password!: string;
+  accountId!: number;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+  accountId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  underscored: true,
+});
+
+User.belongsTo(Account, { foreignKey: 'accountId', as: 'account' });
+
+export default User;
