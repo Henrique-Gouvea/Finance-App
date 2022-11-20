4de5addbb9785c75ae5004b97c92c728ad13d828
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-11-20 08:43:26 -0300

    feat(Docker): Create Docker file and dockercompose

--- backend/build/database/config/database.js
+++ backend/build/database/config/database.js
@@ -1,23 +1,12 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+"use strict";
+require("dotenv/config");
+const config = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'FINANCE_APP',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: process.env.DIALECT,
+    logging: false,
+};
+module.exports = config;
